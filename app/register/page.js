3fe03af420168cb763c0e7fd03754150934e116e
hfazeli <h.fazeli@baramjk.com>
2025-09-17 13:01:44 +0330

    add logo motion

--- app/register/page.js
+++ app/register/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Page = () => {
   return (
-    <div className="page-container pt-[82px]">
+    <div className="page-container ">
       <RegisterForm />
       {/* <div className="absolute bottom-0 start-0 w-[564px] h-[494px] transform -translate-x-[130px] translate-y-[330px] z-[5]">
         <Image src={'/pages-image.png'} alt={''} fill className="object-cover"/>
